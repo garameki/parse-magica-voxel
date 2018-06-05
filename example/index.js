a9f7cac73693c0517b6d8dd3ee50cb5cc238d6e9
garameki <garameki@users.noreply.github.com>
2018-06-05 17:34:37 +0900

    Move example/index.js to src/index.js

--- example/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var parseMagicaVoxel = require('../src/index.js');
-
-fs.readFile("./chr_sword.vox", function (err, Buffer) {
-  if (err) throw err;
-  console.log(JSON.stringify(parseMagicaVoxel(Buffer), null, 2));
-});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var parseMagicaVoxel = require('./parseMagicaVoxel.js');
+
+fs.readFile("./example/aPiece.vox", function (err, Buffer) {
+  if (err) throw err;
+  console.log(JSON.stringify(parseMagicaVoxel(Buffer), null, 2));
+});
